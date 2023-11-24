3964175828a63ddabc2087a755882f1a76da6578
nivleking <c14210307@john.petra.ac.id>
2023-11-23 22:00:04 -0800

    Initial Commit

---
+++ CobaGithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CobaGithub
+//
+//  Created by Kelvin Sidharta Sie on 23/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
